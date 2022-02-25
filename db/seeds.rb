9b3e5cf8e1fca94fc471f3a9f680fa19b80dc2e5
Michelle Knolly <michelleknolly@gmail.com>
2022-02-25 15:49:07 +0100

    Fixed bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,16 +14,16 @@ user1.avatar.attach(io: URI.open('https://res.cloudinary.com/dbzj7fllo/image/upl
 filename: 'avatar1.jpg', content_type: 'image/png')
 user2 = User.create!(email: "tod@tod.com", password: "tod123", first_name: "Tod", last_name: "Sue")
 user2.avatar.attach(io: URI.open('https://res.cloudinary.com/dbzj7fllo/image/upload/v1645790517/development/NY9hnAbp_fc5umz.jpg'),
-filename: 'avatar1.jpg', content_type: 'image/png')
+filename: 'avatar2.jpg', content_type: 'image/png')
 user3 = User.create!(email: "lisa@lisa.com", password: "lisa123", first_name: "Lisa", last_name: "Moon")
 user3.avatar.attach(io: URI.open('https://res.cloudinary.com/dbzj7fllo/image/upload/v1645790349/development/female2_esrwlh.jpg'),
-filename: 'avatar1.jpg', content_type: 'image/png')
+filename: 'avatar3.jpg', content_type: 'image/png')
 user4 = User.create!(email: "nick@nick.com", password: "nick123", first_name: "Nick", last_name: "Sun")
 user4.avatar.attach(io: URI.open('https://res.cloudinary.com/dbzj7fllo/image/upload/v1645790517/development/86_cyfljg.jpg'),
-filename: 'avatar1.jpg', content_type: 'image/png')
+filename: 'avatar4.jpg', content_type: 'image/png')
 user5 = User.create!(email: "kurt@kurt.com", password: "kurt123", first_name: "Kurt", last_name: "Cloud")
 user5.avatar.attach(io: URI.open('https://res.cloudinary.com/dbzj7fllo/image/upload/v1645790517/development/AW-rdWlG_ag3u6f.jpg'),
-filename: 'avatar1.jpg', content_type: 'image/png')
+filename: 'avatar5.jpg', content_type: 'image/png')
 
 # Category seeds
 puts "Creating categories"
@@ -53,7 +53,7 @@ equip3.photo.attach(io: URI.open('https://res.cloudinary.com/dbzj7fllo/image/upl
 equip3.save!
 equip4 = EquipmentItem.new(category: cat2, title: "Man's snowboard",
                            price: 48.00, location: "Sonthofen", user_id: user2.id,
-                           description: "Loved doing tricks on this board, broke my leg cant use at the moment")
+                           description: "Loved doing tricks on this board, broke my leg can't use at the moment")
 equip4.photo.attach(io: URI.open('https://res.cloudinary.com/dbzj7fllo/image/upload/v1645545208/development/rsaaygbf7j5ttyibnidqbgdxcpay.jpg'),
                     filename: 'snowboard2.jpg', content_type: 'image/png')
 equip4.save!
@@ -65,7 +65,7 @@ equip5.photo.attach(io: URI.open('https://res.cloudinary.com/dbzj7fllo/image/upl
 equip5.save!
 equip6 = EquipmentItem.new(category: cat3, title: "Womans Ski Goggles",
                            price: 15.15, location: "Zurich", user_id: user3.id,
-                           description: "these are some awesome goggles to take skiing")
+                           description: "These are some awesome goggles to take skiing")
 equip6.photo.attach(io: URI.open('https://res.cloudinary.com/dbzj7fllo/image/upload/v1645706772/development/02pmdkw9ng8j2u1qgt6r37w2vyuw.jpg'),
                     filename: 'goggles.jpg', content_type: 'image/png')
 equip6.save!
@@ -75,7 +75,7 @@ puts "Creating bookings"
 Booking.create!(user_id: user5.id, equipment_item_id: equip2.id, booking_date: Date.today)
 Booking.create!(user_id: user3.id, equipment_item_id: equip3.id, booking_date: Date.today)
 Booking.create!(user_id: user2.id, equipment_item_id: equip5.id, booking_date: Date.today)
-Booking.create!(user_id: user1.id, equipment_item_id: equip1.id, booking_date: Date.today)
+Booking.create!(user_id: user1.id, equipment_item_id: equip6.id, booking_date: Date.today)
 Booking.create!(user_id: user4.id, equipment_item_id: equip4.id, booking_date: Date.today)
 
 puts 'All done seeding'
